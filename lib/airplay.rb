7b2c46be21075f28f8a29ebb0731e2dc9c305b38
Bruno Aguirre <yo@brunoaguirre.com>
2011-08-17 16:55:10 -0300

    Removed versioning in the module

--- lib/airplay.rb
+++ lib/airplay.rb
@@ -2,7 +2,6 @@ require 'dnssd'
 require 'net/http'
 require 'uri'
 
-require 'airplay/version'
 require 'airplay/protocol'
 require 'airplay/protocol/image'
 require 'airplay/protocol/video'
--- lib/airplay/version.rb
+++
@@ -1,3 +0,0 @@
-module Airplay
-  VERSION = 0.1.1
-end
