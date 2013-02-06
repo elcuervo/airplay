15bc8a987e6a9d1c1e5af0e7bed2b44f9d758407
Bruno Aguirre <yo@brunoaguirre.com>
2013-02-06 18:34:50 -0200

    Fixes image broadcasting

--- lib/airplay/protocol/image.rb
+++ lib/airplay/protocol/image.rb
@@ -6,12 +6,15 @@ module Airplay::Protocol
   class Image
     def initialize(media_or_io, options = {})
       @content = get_content(media_or_io)
+      @transition = options.fetch(:transition, "None")
     end
 
     # Public: Broadcasts the content to the device
     #
     def broadcast
-      Airplay.connection.post("/photo", @content)
+      Airplay.connection.put("/photo", @content, {
+        "X-Apple-Transition" => @transition
+      })
     end
 
     private
@@ -24,12 +27,12 @@ module Airplay::Protocol
       case true
       when File.exists?(File.expand_path(media_or_io))
         File.read(media_or_io)
+      when !!(media_or_io =~ URI::regexp)
+        open(media_or_io).read
       when media_or_io.is_a?(String)
         media_or_io
       when media_or_io.respond_to?(:read)
         media_or_io.read
-      when !!(media_or_io =~ URI::regexp)
-        open(media_or_io).read
       end
     end
   end
