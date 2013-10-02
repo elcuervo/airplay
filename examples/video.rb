1dd605556526045139ebafd9a8c7b988f39443a1
Bruno Aguirre <yo@brunoaguirre.com>
2013-10-02 03:42:12 -0300

    Updates example

--- examples/video.rb
+++ examples/video.rb
@@ -1,25 +1,24 @@
 require "airplay"
 
 Airplay.configure do |c|
-  c.log_level = "debug"
+#  c.log_level = "debug"
 end
-
-Airplay.use "corax"
 
 video = "http://trailers.apple.com/movies/marvel/ironman3/ironman3-tlr1-m4mb0_h1080p.mov"
 
 Airplay.play video
 puts "Starts playing: #{video}"
+Airplay.player.progress -> info {
+  if info["rate"]
+    if info["readyToPlay"]
+      total = info["duration"]
+      current = info["position"]
+      percent = (current*100/total).round
 
-Airplay.player.progress do |status|
-  if status["readyToPlay"]
-    total = status["duration"]
-    current = status["position"]
-    percent = (current*100/total).round
-
-    print "\r|#{"=" * percent}> #{percent}%"
+      print "\r|#{"=" * percent}> #{percent}%"
+    end
   end
-end
+}
 
 Airplay.player.wait
 puts "Video playback finished!"
