54370cba98ac5e062aacfc546820bbb640fbebd1
Bruno Aguirre <yo@brunoaguirre.com>
2013-10-22 21:02:35 -0200

    Removes deprecated class

--- lib/airplay/structure.rb
+++
@@ -1,7 +0,0 @@
-# Public: Stupid class to easily create and initialize a Struct from a hash
-#
-class Structure < Struct
-  def self.create(hash)
-    new(*members.map { |member| hash[member.to_sym] })
-  end
-end
