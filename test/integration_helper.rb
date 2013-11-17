04b7f1b3077e7c2c7aa9a303bdabd26cbb8e4d16
elCuervo <yo@brunoaguirre.com>
2013-11-17 17:03:32 -0200

    Use co-located apple tv

--- test/integration_helper.rb
+++ test/integration_helper.rb
@@ -2,8 +2,15 @@ require "test_helper"
 require "airplay"
 require "celluloid/autostart"
 
+Airplay.configure { |c| c.autodiscover = false }
+Airplay.devices.add("Block TV", "blocktv.airplay.io:7000")
+
 def test_device
-  @_device ||= Airplay["Block TV"]
+  @_device ||= begin
+    device = Airplay["Block TV"]
+    device.password = ENV["TEST_TV_PASSWORD"]
+    device
+  end
 end
 
 def sample_images
