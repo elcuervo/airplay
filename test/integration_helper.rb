3bac23df3d0a05c8dd173eca1e5a2416b040bc70
elCuervo <yo@brunoaguirre.com>
2013-11-26 16:26:33 -0300

    Allows to use a custom url for the test tv

--- test/integration_helper.rb
+++ test/integration_helper.rb
@@ -2,8 +2,9 @@ require "test_helper"
 require "airplay"
 require "celluloid/autostart"
 
+device = ENV.fetch("TEST_TV_URL", "blocktv.airplay.io:7000")
 Airplay.configure { |c| c.autodiscover = false }
-Airplay.devices.add("Block TV", "blocktv.airplay.io:7000")
+Airplay.devices.add("Block TV", device)
 
 def test_device
   @_device ||= begin
@@ -15,4 +16,8 @@ end
 
 def sample_images
   @_images ||= Dir.glob("test/fixtures/files/*.png")
+end
+
+def sample_video
+  @_video ||= "http://movietrailers.apple.com/movies/universal/rush/rush-tlr3_480p.mov"
 end
